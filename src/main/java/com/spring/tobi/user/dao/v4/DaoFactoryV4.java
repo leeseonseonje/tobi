95e55d7821054efa98d314b58a68b290c6a98cf8
leeseonseonje <leeseonje9323@gmail.com>
2022-05-06 23:17:25 +0900

    h2, maria 등록

--- src/main/java/com/spring/tobi/user/dao/v4/DaoFactoryV4.java
+++ src/main/java/com/spring/tobi/user/dao/v4/DaoFactoryV4.java
@@ -8,12 +8,22 @@ import org.springframework.context.annotation.Configuration;
 public class DaoFactoryV4 {
 
     @Bean
-    public UserDaoV4 userDao() {
-        return new UserDaoV4(connectionMaker());
+    public UserDaoV4 userDaoMaria() {
+        return new UserDaoV4(connectionMaria());
     }
 
     @Bean
-    public ConnectionMaker connectionMaker() {
+    public ConnectionMaker connectionMaria() {
         return new MariaConnectionV4();
+    }
+
+    @Bean
+    public UserDaoV4 userDaoH2() {
+        return new UserDaoV4((connectionH2()));
+    }
+
+    @Bean
+    public ConnectionMaker connectionH2() {
+        return new H2ConnectionV4();
     }
 }
--- src/test/java/com/spring/tobi/user/UserDaoTestV4.java
+++ src/test/java/com/spring/tobi/user/UserDaoTestV4.java
@@ -3,28 +3,40 @@ package com.spring.tobi.user;
 import com.spring.tobi.user.dao.v4.DaoFactoryV4;
 import com.spring.tobi.user.dao.v4.UserDaoV4;
 import com.spring.tobi.user.domain.User;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.BeanFactory;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import java.sql.SQLException;
 
+@Slf4j
 @SpringBootTest
 public class UserDaoTestV4 {
 
-    UserDaoV4 userDao;
+    UserDaoV4 userDaoMaria;
+    UserDaoV4 userDaoH2;
 
     @BeforeEach
     void init() throws SQLException, ClassNotFoundException {
-        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(DaoFactoryV4.class);
-        userDao = context.getBean("userDao", UserDaoV4.class);
-        this.userDao.delete();
+        ApplicationContext context = new AnnotationConfigApplicationContext(DaoFactoryV4.class);
+        userDaoMaria = context.getBean("userDaoMaria", UserDaoV4.class);
+        userDaoMaria.delete();
+
+        userDaoH2 = context.getBean("userDaoH2", UserDaoV4.class);
+        userDaoH2.delete();
     }
 
     @Test
     void test() throws SQLException, ClassNotFoundException {
+        crud(userDaoMaria);
+        crud(userDaoH2);
+    }
+
+    private void crud(UserDaoV4 userDao) throws SQLException, ClassNotFoundException {
         User user = new User();
         user.setId("seon");
         user.setName("이선제");
