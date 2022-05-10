8f4e074a350fd53a132ac2a056e3ffa93832d4be
leeseonseonje <leeseonje9323@gmail.com>
2022-05-10 23:28:15 +0900

    JUnit TEsT

--- src/main/java/com/spring/tobi/user/dao/v5/UserDaoV5.java
+++ src/main/java/com/spring/tobi/user/dao/v5/UserDaoV5.java
@@ -66,4 +66,20 @@ public class UserDaoV5 {
         ps.close();
         c.close();
     }
+
+    public int getCount() throws SQLException {
+        Connection c = dataSource.getConnection();
+
+        PreparedStatement ps = c.prepareStatement("select count(*) from users");
+
+        ResultSet rs = ps.executeQuery();
+        rs.next();
+        int count = rs.getInt(1);
+
+        rs.close();
+        ps.close();
+        c.close();
+
+        return count;
+    }
 }
---
+++ src/test/java/com/spring/tobi/chapter2test/UserDaoTest.java
@@ -0,0 +1,40 @@
+package com.spring.tobi.chapter2test;
+
+import com.spring.tobi.user.dao.v5.DaoFactoryV5;
+import com.spring.tobi.user.dao.v5.UserDaoV5;
+import com.spring.tobi.user.domain.User;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.sql.SQLException;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class UserDaoTest {
+
+    @Test
+    void addAndGet() throws SQLException, ClassNotFoundException {
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(DaoFactoryV5.class);
+
+        UserDaoV5 userDao = applicationContext.getBean("userDaoH2DataSource", UserDaoV5.class);
+
+        userDao.delete();
+        int count = userDao.getCount();
+
+        assertThat(count).isEqualTo(0);
+
+        User user = new User();
+        user.setId("yeongdonge");
+        user.setName("김동영");
+        user.setPassword("cute");
+
+        userDao.add(user);
+
+        User user2 = userDao.get(user.getId());
+
+        assertThat(user2.getName()).isEqualTo(user.getName());
+        assertThat(user2.getPassword()).isEqualTo(user.getPassword());
+    }
+}
