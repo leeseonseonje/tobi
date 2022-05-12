85d2f6aed6a6d1aa1aee9491320db80f5b609b58
leeseonseonje <leeseonje9323@gmail.com>
2022-05-12 23:45:41 +0900

    mariadb Connection Pool

--- src/main/java/com/spring/tobi/user/dao/v5/DaoFactoryV5.java
+++ src/main/java/com/spring/tobi/user/dao/v5/DaoFactoryV5.java
@@ -1,5 +1,6 @@
 package com.spring.tobi.user.dao.v5;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
@@ -11,11 +12,24 @@ public class DaoFactoryV5 {
 
     @Bean
     public UserDaoV5 userDaoH2DataSource() {
-        return new UserDaoV5(dataSource());
+        return new UserDaoV5(h2());
     }
 
     @Bean
-    public DataSource dataSource() {
+    public DataSource h2() {
         return new DriverManagerDataSource("jdbc:h2:tcp://localhost/~/tobi", "sa", "");
+    }
+
+    @Bean
+    public UserDaoV5 userDaoMariaDataSource() {
+        return new UserDaoV5(maria());
+    }
+    @Bean
+    public DataSource maria() {
+        HikariDataSource dataSource = new HikariDataSource();
+        dataSource.setJdbcUrl("jdbc:mariadb://localhost:3306/tobi");
+        dataSource.setUsername("root");
+        dataSource.setPassword("mariadb");
+        return dataSource;
     }
 }
